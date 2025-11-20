771b1ad6ea77a7b8bf446f310139ea82964d93e8
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-11-20 18:57:44 +0400

    draft

--- crates/tno-observe/src/logger/object/mod.rs
+++ crates/tno-observe/src/logger/object/mod.rs
@@ -1,9 +1,11 @@
 pub mod format;
-pub mod level;
-pub mod rfc3339;
-pub mod timezone;
-
 pub use format::LoggerFormat;
+
+pub mod level;
 pub use level::LoggerLevel;
+
+pub mod rfc3339;
 pub use rfc3339::LoggerRfc3339;
+
+pub mod timezone;
 pub use timezone::{LoggerTimeZone, init_local_offset};
