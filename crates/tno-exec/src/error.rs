82e548b11bcd2fe2a0d7ed5b1eb58f93bbd65604
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-11-27 10:43:30 +0400

    draft

--- crates/tno-exec/src/error.rs
+++ crates/tno-exec/src/error.rs
@@ -8,9 +8,6 @@ pub enum ExecError {
         actual: String,
     },
 
-    #[error("function not found in registry: {0}")]
-    FunctionNotFound(String),
-
     #[error("invalid specification: {0}")]
     InvalidSpec(String),
 
@@ -24,7 +21,7 @@ pub enum ExecError {
     Signal,
 
     #[error("io error: {0}")]
-    Io(String),
+    Io(#[from] std::io::Error),
 
     #[error("internal error: {0}")]
     Internal(String),
